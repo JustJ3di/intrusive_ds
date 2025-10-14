b5df4c57a51f1f8f57fc638e37bd4860eb4a6348
JustJ3di <hermo@DESKTOP-2Q2RO5F.localdomain>
2025-10-14 23:28:53 +0200

    add container of (access to struct member as linux kernel)

--- utils.h
+++ utils.h
@@ -3,6 +3,13 @@
 
 #define  NULL ((void *)0)
 
+#define offsetof(TYPE, MEMBER) ((unsigned long) &((TYPE *)0)->MEMBER)
+#define container_of(ptr, type, member) ({ \
+    (type *)( (char *)(ptr) - offsetof(type, member) ); \
+}
+
 typedef enum{false ,true}bool;
+
+
 
 #endif
