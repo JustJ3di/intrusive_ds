804db7ceec7c4dbdea417951661c0adcb3b75da8
JustJ3di <hermo@DESKTOP-2Q2RO5F.localdomain>
2025-10-15 16:38:44 +0200

    readme update

--- utils.h
+++ utils.h
@@ -6,7 +6,7 @@
 #define offsetof(TYPE, MEMBER) ((unsigned long) &((TYPE *)0)->MEMBER)
 #define container_of(ptr, type, member) ({ \
     (type *)( (char *)(ptr) - offsetof(type, member) ); \
-}
+})\
 
 typedef enum{false ,true}bool;
 
